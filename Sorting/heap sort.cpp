940411d1f7253d0e63bce1e483bb11fde61ba1f4
tanyalava <50517390+tanyalava@users.noreply.github.com>
2021-10-10 19:43:25 +0530

    Add files via upload

---
+++ Sorting/heap sort.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+void heapify(int a[],int n,int i)
+{
+	int largest = i;
+	int l = 2*i + 1 ;
+	int r = 2*i + 2 ;
+	if (l < n && a[l] > a[largest])
+        largest = l;
+ 
+    if (r < n && a[r] > a[largest])
+        largest = r;
+ 
+    if (largest != i) 
+	{
+        swap(a[i], a[largest]);
+        heapify(a, n, largest);
+    }
+}
+void heapsort(int a[],int n)
+{
+	for(int i=n/2-1;i>=0;i--)
+	{
+		heapify(a,n,i);
+	}
+	for(int i=n-1;i>=0;i--)
+	{
+		swap(a[0],a[i]);
+		heapify(a,i,0);
+	}
+}
+int main()
+{
+	int n;
+	cout<<"Enter the array size\n";
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+	{
+		cin>>a[i];
+	}
+	heapsort(a,n);
+	cout<<"Array after Heap sort\n";
+	for(int i=0;i<n;i++)
+	{
+		cout<<a[i]<<" ";
+	}
+}
